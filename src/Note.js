99afea8e7f8a5303c818692ea054b1bb8e31dc91
fans656 <fans656@yahoo.com>
2018-09-02 23:46:11 +0800

    Add note link

--- src/Note.js
+++ src/Note.js
@@ -5,14 +5,25 @@ export default class NoteList extends React.Component {
   render() {
     const note = this.props.note;
     return (
-      <pre style={inListStyle}>
-        {JSON.stringify(note, null, 2)}
-      </pre>
+      <div className="note" style={inListNoteStyle}>
+        <pre>
+          {JSON.stringify(note, null, 2)}
+        </pre>
+        <div className="actions left-right" style={actionsStyle}>
+          <div className="left">
+          </div>
+          <div className="right">
+            <a href={`/note/${note.id}`} target="_blank">
+              {note.ctime}
+            </a>
+          </div>
+        </div>
+      </div>
     );
   }
 }
 
-const inListStyle = {
+const inListNoteStyle = {
   background: 'white',
   boxShadow: '0 0 5px #ddd',
   fontFamily: 'Consolas',
@@ -20,4 +31,9 @@ const inListStyle = {
   color: '#333',
   padding: '2em',
   width: '45rem',
+  marginBottom: '1rem',
+};
+
+const actionsStyle = {
+  color: '#ccc',
 };
